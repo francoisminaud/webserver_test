d9a41680a198b760ced18ee8907fc5cb8337d127
Francois Minaud <francois.minaud@gmail.com>
2020-10-18 16:31:38 +0200

    Now parse the input parameters

--- wiremind_webserver.py
+++ wiremind_webserver.py
@@ -4,9 +4,16 @@ from http.server import HTTPServer, BaseHTTPRequestHandler
 class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
 
     def do_GET(self):
+        path = self.path
+        if "firstname" not in path or "lastname" not in path:
+            message = "Greetings stranger !\n"
+        else:
+            firstname = path.split("&")[0].split("=")[1]
+            lastname = path.split("&")[1].split("=")[1]
+            message = "Greetings " + firstname + " " + lastname + "!\n"
         self.send_response(200)
         self.end_headers()
-        self.wfile.write(b'Hello Wiremind!\n')
+        self.wfile.write(message.encode("utf-8"))
 
 
 httpd = HTTPServer(('0.0.0.0', 8001), SimpleHTTPRequestHandler)
